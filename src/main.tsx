54767a0b41476b4237023d1bcdba7fd749fc430f
AJ Rishad <93240576+GeniusBengali@users.noreply.github.com>
2025-05-21 23:09:20 +0600

    fix typescript build error

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,18 @@ import AppRoutes from './routes.tsx'
 import {Toaster} from "sonner";
 import {AuthContextProvider} from "./context/auth-provider.tsx";
 import {AppContextProvider} from "./context/app-provider.tsx";
-import {backButton, enableClosingConfirmation, init, setMiniAppHeaderColor} from "@telegram-apps/sdk-react";
+import {backButton, closingBehavior, init} from "@telegram-apps/sdk-react";
 
 // const teleWebApp = window.Telegram.WebApp
 
 if(import.meta.env.PROD){
   init()
-  backButton.mount()
-  enableClosingConfirmation()
-  setMiniAppHeaderColor("#161616")
+  if(backButton.mount.isAvailable()){
+    backButton.mount()
+  }
+  if(closingBehavior.mount.isAvailable()){
+    closingBehavior.mount()
+  }
 }
 
 createRoot(document.getElementById('root')!).render(
